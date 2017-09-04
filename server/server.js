02922b6798b78761c990bf0afd1b434a2e5ca2f5
Utku Kaan <tutkuceren15@gmail.com>
2017-09-04 23:04:54 +0300

    Add POST /todo route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {user} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todo', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.status(201).send(doc);
+    }, (e) => {
+        e.serverMessage = "Failed to satisfy model requirements"
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
