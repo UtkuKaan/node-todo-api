4db809483cbaff97e8fb13fe67be5e71a72ef515
Utku Kaan <tutkuceren15@gmail.com>
2017-09-05 03:08:07 +0300

    Add GET /todo route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todo', (req, res) => {
     });
 });
 
+app.get('/todo', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    }   );
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
