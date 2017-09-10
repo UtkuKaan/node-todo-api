871e1ada4edd8e8b6ce1312738b030622fe4d9d7
Utku Kaan <tutkuceren15@gmail.com>
2017-09-10 14:16:31 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
